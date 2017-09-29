0ee9a37c00167db3407cef4224b07a3d80b4a0a4
aneeshChivukula <c.s.aneesh@gmail.com>
2017-09-29 18:42:00 +1000

    granger causality commit

--- financedataanalysis/src/regression/MSE.py
+++ financedataanalysis/src/regression/MSE.py
@@ -15,43 +15,61 @@ import math
 import itertools
 
 import collections
+import pickle
+
+QmemodelrmaePath = "/home/achivuku/PycharmProjects/financedataanalysis/qmemodelrmae.pkl"
+QmemodelrmsePath = "/home/achivuku/PycharmProjects/financedataanalysis/qmemodelrmse.pkl"
+QmemodelurmaePath = "/home/achivuku/PycharmProjects/financedataanalysis/qmemodelurmae.pkl"
+QmemodelurmsePath = "/home/achivuku/PycharmProjects/financedataanalysis/qmemodelurmse.pkl"
+
+MsemodelrmaePath = "/home/achivuku/PycharmProjects/financedataanalysis/msemodelrmae.pkl"
+MsemodelrmsePath = "/home/achivuku/PycharmProjects/financedataanalysis/msemodelrmse.pkl"
+MsemodelurmaePath = "/home/achivuku/PycharmProjects/financedataanalysis/msemodelurmae.pkl"
+MsemodelurmsePath = "/home/achivuku/PycharmProjects/financedataanalysis/msemodelurmse.pkl"
+
 
 
 df = pd.read_csv("/home/achivuku/Documents/financedataanalysis/pricesvolumes.csv")
+# print(df.columns)
+# sys.exit()
 
-cols = [2,4,6,8,10,12,14,16,18,20,22,24,26,28,30,32,33,34,36,38,40,42]
+# cols = [2,4,6,8,10,12,14,16,18,20,22,24,26,28,30,32,33,34,36,38,40,42]
+cols = [1,2,3,4,6,8,10,12,14,16,18,20,21,22,23,24,26,28,30,32,33,34,36,38,40,42]
+
 df.drop(df.columns[cols],axis=1,inplace=True)
-df['Date'] = pd.to_datetime(df['Date'])
+# df['Date'] = pd.to_datetime(df['Date'])
 
 print(len(df.columns)) # 21
 print((df.columns)) # Index([u'Date', u'^DJI_prices', u'^GSPC_prices', u'^IXIC_prices', u'AAPL_prices', u'ABT_prices', u'AEM_prices', u'AFG_prices', u'APA_prices', u'B_prices', u'CAT_prices', u'FRD_prices', u'GIGA_prices', u'LAKE_prices', u'MCD_prices', u'MSFT_prices', u'ORCL_prices', u'SUN_prices', u'T_prices', u'UTX_prices', u'WWD_prices'], dtype='object')
 print(len(df.index)) # 5285
 
 allcols = df.columns.tolist()
+print('allcols',allcols[1:])
+df[allcols[1:]] = df[allcols[1:]].apply(pd.to_numeric).apply(lambda x: x/x.mean(), axis=0)
+
 allcols.remove("Date")
-allcols.remove("DJI_prices")
-allcols.remove("FRD_prices")
-allcols.remove("GSPC_prices")
-allcols.remove("GIGA_prices")
+# allcols.remove("DJI_prices")
+# allcols.remove("FRD_prices")
+# allcols.remove("GSPC_prices")
+# allcols.remove("GIGA_prices")
 
-
-print('allcols',allcols)
 
 # print(len(df.columns))
 # print(df['Date'])
 
-inputbatchsize = 5100
-p = 100
-q = 100
+inputbatchsize = 5000
+p = 200
+q = 200
 
 percentilenum = 10
+numepochs = 10
 
-def getstockdata(dfone):
+def getstockdata(dfone, lag):
     Ypast = []
     Ycurr = []
     for i in xrange(-inputbatchsize, 0):
         y = dfone.iloc[i,1]
-        x = dfone.iloc[i - p:i,1].tolist()
+        x = dfone.iloc[i - lag:i,1].tolist()
         Ypast.append(x)
         Ycurr.append(y)
     Ypast = np.vstack(Ypast)
@@ -152,79 +170,88 @@ def restricted_qme_model(lag):
     model.compile(optimizer='adam', loss=quadratic_mean_error, metrics=['mae'])
     return model
 
-qmemodelrmae = collections.defaultdict(dict)
-qmemodelrmse = collections.defaultdict(dict)
-qmemodelurmae = collections.defaultdict(dict)
-qmemodelurmse = collections.defaultdict(dict)
-
-# for stock1,stock2 in itertools.combinations(allcols,2):
-#     qmemodelrmae[stock1][stock2] = 0
-#     qmemodelrmse[stock1][stock2] = 0
+# qmemodelrmae = collections.defaultdict(dict)
+# qmemodelrmse = collections.defaultdict(dict)
+# qmemodelurmae = collections.defaultdict(dict)
+# qmemodelurmse = collections.defaultdict(dict)
 #
-#     qmemodelurmae[stock1][stock2] = 0
-#     qmemodelurmse[stock1][stock2] = 0
-
-for stock1,stock2 in itertools.combinations(allcols,2):
-    print('stock1,stock2',stock1,stock2)
-    Ypast, Ycurr = getstockdata(df[['Date', stock1]])
-    # Ypast, Ycurr = getstockdata(df[['Date', 'MSFT_prices']])
-    numrecords = len(Ycurr)
-    numtestrecords = int(math.ceil(0.3*numrecords))
-    numtrainrecords = int(math.ceil(0.7*numrecords))
-
-    modelr = restricted_qme_model(p)
-    np.random.seed(3)
-    modelr.fit(Ypast[:numtrainrecords], Ycurr[:numtrainrecords], epochs=5, batch_size=32, verbose=2, validation_split=0.1)
-    Ycurrp = modelr.predict(Ypast[-numtestrecords:], batch_size=128)
-    qme_mse_valuer, qme_mae_valuer = modelr.evaluate(Ypast[-numtestrecords:], Ycurr[-numtestrecords:], batch_size=128, verbose=1)
-
-    print('\n')
-    print('qme modelr Ycurrp.mean()',Ycurrp.mean())
-    print('qme modelr Ycurrp.std()',Ycurrp.std())
-    print('qme modelr mae_value',qme_mae_valuer)
-    print('qme modelr mse_value',qme_mse_valuer)
-    print('qme modelr r2_score(Ycurrp, Ycurr)',r2_score(Ycurrp, Ycurr[-numtestrecords:]))
-
-    qmemodelrmae[stock1][stock2] = qme_mae_valuer
-    qmemodelrmse[stock1][stock2] = qme_mse_valuer
-
-    Ypast1, Ycurr1 = getstockdata(df[['Date', stock1]])
-    Ypast2, Ycurr2 = getstockdata(df[['Date', stock2]])
-    Ycurr2 = Ycurr1
-
-    Ypast = np.concatenate((Ypast1, Ypast2))
-    Ycurr = np.concatenate((Ycurr1, Ycurr2))
-
-    # Ypast,Ycurr = getstockdata(df[['Date', stock2]])
-    # Ypast,Ycurr = getstockdata(df[['Date','ORCL_prices']])
-    numrecords = len(Ycurr)
-    numtestrecords = int(math.ceil(0.3*numrecords))
-    numtrainrecords = int(math.ceil(0.7*numrecords))
-
-    modelur = restricted_qme_model(q)
-    np.random.seed(7)
-    modelur.fit(Ypast[:numtrainrecords], Ycurr[:numtrainrecords], epochs=5, batch_size=32, verbose=2, validation_split=0.1)
-    Ycurrp = modelur.predict(Ypast[-numtestrecords:], batch_size=128)
-    qme_mse_valueur, qme_mae_valueur = modelur.evaluate(Ypast[-numtestrecords:], Ycurr[-numtestrecords:], batch_size=128, verbose=1)
-
-    print('\n')
-    print('qme modelur Ycurrp.mean()',Ycurrp.mean())
-    print('qme modelur Ycurrp.std()',Ycurrp.std())
-    print('qme modelur mae_value',qme_mae_valueur)
-    print('qme modelur mse_value',qme_mse_valueur)
-    print('qme modelur r2_score(Ycurrp, Ycurr)',r2_score(Ycurrp, Ycurr[-numtestrecords:]))
-
-    qmemodelurmae[stock1][stock2] = qme_mae_valueur
-    qmemodelurmse[stock1][stock2] = qme_mse_valueur
-
-
-print('qmemodelrmae',qmemodelrmae)
-print('qmemodelrmse',qmemodelrmse)
-print('qmemodelurmae',qmemodelurmae)
-print('qmemodelurmse',qmemodelurmse)
-
-
-
+# # for stock1,stock2 in itertools.combinations(allcols,2):
+# #     qmemodelrmae[stock1][stock2] = 0
+# #     qmemodelrmse[stock1][stock2] = 0
+# #
+# #     qmemodelurmae[stock1][stock2] = 0
+# #     qmemodelurmse[stock1][stock2] = 0
+#
+# for stock1,stock2 in itertools.combinations(allcols,2):
+#     print('stock1,stock2',stock1,stock2)
+#     Ypast, Ycurr = getstockdata(df[['Date', stock1]], p)
+#     # Ypast, Ycurr = getstockdata(df[['Date', 'MSFT_prices']])
+#     numrecords = len(Ycurr)
+#     numtestrecords = int(math.ceil(0.3*numrecords))
+#     numtrainrecords = int(math.ceil(0.7*numrecords))
+#
+#     modelr = restricted_qme_model(p)
+#     np.random.seed(3)
+#     modelr.fit(Ypast[:numtrainrecords], Ycurr[:numtrainrecords], epochs=numepochs, batch_size=32, verbose=2, validation_split=0.1)
+#     Ycurrp = modelr.predict(Ypast[-numtestrecords:], batch_size=128)
+#     qme_mse_valuer, qme_mae_valuer = modelr.evaluate(Ypast[-numtestrecords:], Ycurr[-numtestrecords:], batch_size=128, verbose=1)
+#
+#     print('\n')
+#     print('qme modelr Ycurrp.mean()',Ycurrp.mean())
+#     print('qme modelr Ycurrp.std()',Ycurrp.std())
+#     print('qme modelr mae_value',qme_mae_valuer)
+#     print('qme modelr mse_value',qme_mse_valuer)
+#     print('qme modelr r2_score(Ycurrp, Ycurr)',r2_score(Ycurrp, Ycurr[-numtestrecords:]))
+#
+#     qmemodelrmae[stock1][stock2] = qme_mae_valuer
+#     qmemodelrmse[stock1][stock2] = qme_mse_valuer
+#
+#     Ypast1, Ycurr1 = getstockdata(df[['Date', stock1]], p)
+#     Ypast2, Ycurr2 = getstockdata(df[['Date', stock2]], q)
+#     Ycurr2 = Ycurr1
+#
+#     Ypast = np.concatenate((Ypast1, Ypast2))
+#     Ycurr = np.concatenate((Ycurr1, Ycurr2))
+#
+#     # Ypast,Ycurr = getstockdata(df[['Date', stock2]])
+#     # Ypast,Ycurr = getstockdata(df[['Date','ORCL_prices']])
+#     numrecords = len(Ycurr)
+#     numtestrecords = int(math.ceil(0.3*numrecords))
+#     numtrainrecords = int(math.ceil(0.7*numrecords))
+#
+#     modelur = restricted_qme_model(q)
+#     np.random.seed(7)
+#     modelur.fit(Ypast[:numtrainrecords], Ycurr[:numtrainrecords], epochs=numepochs, batch_size=32, verbose=2, validation_split=0.1)
+#     Ycurrp = modelur.predict(Ypast[-numtestrecords:], batch_size=128)
+#     qme_mse_valueur, qme_mae_valueur = modelur.evaluate(Ypast[-numtestrecords:], Ycurr[-numtestrecords:], batch_size=128, verbose=1)
+#
+#     print('\n')
+#     print('qme modelur Ycurrp.mean()',Ycurrp.mean())
+#     print('qme modelur Ycurrp.std()',Ycurrp.std())
+#     print('qme modelur mae_value',qme_mae_valueur)
+#     print('qme modelur mse_value',qme_mse_valueur)
+#     print('qme modelur r2_score(Ycurrp, Ycurr)',r2_score(Ycurrp, Ycurr[-numtestrecords:]))
+#
+#     qmemodelurmae[stock1][stock2] = qme_mae_valueur
+#     qmemodelurmse[stock1][stock2] = qme_mse_valueur
+#
+#
+# print('qmemodelrmae',qmemodelrmae)
+# print('qmemodelrmse',qmemodelrmse)
+# print('qmemodelurmae',qmemodelurmae)
+# print('qmemodelurmse',qmemodelurmse)
+#
+# with open(QmemodelrmaePath, 'wb') as handle:
+#     pickle.dump(qmemodelrmae, handle, protocol=pickle.HIGHEST_PROTOCOL)
+#
+# with open(QmemodelrmsePath, 'wb') as handle:
+#     pickle.dump(qmemodelrmse, handle, protocol=pickle.HIGHEST_PROTOCOL)
+#
+# with open(QmemodelurmaePath, 'wb') as handle:
+#     pickle.dump(qmemodelurmae, handle, protocol=pickle.HIGHEST_PROTOCOL)
+#
+# with open(QmemodelurmsePath, 'wb') as handle:
+#     pickle.dump(qmemodelurmse, handle, protocol=pickle.HIGHEST_PROTOCOL)
 
 
 msemodelrmae = collections.defaultdict(dict)
@@ -234,7 +261,7 @@ msemodelurmse = collections.defaultdict(dict)
 
 for stock1,stock2 in itertools.combinations(allcols,2):
     print('stock1,stock2',stock1,stock2)
-    Ypast, Ycurr = getstockdata(df[['Date', stock1]])
+    Ypast, Ycurr = getstockdata(df[['Date', stock1]],p)
     # Ypast,Ycurr = getstockdata(df[['Date','MSFT_prices']])
     numrecords = len(Ycurr)
     numtestrecords = int(math.ceil(0.3*numrecords))
@@ -242,7 +269,7 @@ for stock1,stock2 in itertools.combinations(allcols,2):
 
     modelr = restricted_mse_model(p)
     np.random.seed(3)
-    modelr.fit(Ypast[:numtrainrecords], Ycurr[:numtrainrecords], epochs=5, batch_size=32, verbose=2, validation_split=0.1)
+    modelr.fit(Ypast[:numtrainrecords], Ycurr[:numtrainrecords], epochs=numepochs, batch_size=32, verbose=2, validation_split=0.1)
     Ycurrp = modelr.predict(Ypast[-numtestrecords:], batch_size=128)
     mse_mse_valuer, mse_mae_valuer = modelr.evaluate(Ypast[-numtestrecords:], Ycurr[-numtestrecords:], batch_size=128, verbose=1)
 
@@ -256,8 +283,8 @@ for stock1,stock2 in itertools.combinations(allcols,2):
     msemodelrmae[stock1][stock2] = mse_mae_valuer
     msemodelrmse[stock1][stock2] = mse_mse_valuer
 
-    Ypast1, Ycurr1 = getstockdata(df[['Date', stock1]])
-    Ypast2, Ycurr2 = getstockdata(df[['Date', stock2]])
+    Ypast1, Ycurr1 = getstockdata(df[['Date', stock1]],p)
+    Ypast2, Ycurr2 = getstockdata(df[['Date', stock2]],q)
     Ycurr2 = Ycurr1
 
     Ypast = np.concatenate((Ypast1, Ypast2))
@@ -271,7 +298,7 @@ for stock1,stock2 in itertools.combinations(allcols,2):
 
     modelur = restricted_mse_model(q)
     np.random.seed(7)
-    modelur.fit(Ypast[:numtrainrecords], Ycurr[:numtrainrecords], epochs=5, batch_size=32, verbose=2, validation_split=0.1)
+    modelur.fit(Ypast[:numtrainrecords], Ycurr[:numtrainrecords], epochs=numepochs, batch_size=32, verbose=2, validation_split=0.1)
     Ycurrp = modelur.predict(Ypast[-numtestrecords:], batch_size=128)
     mse_mse_valueur, mse_mae_valueur = modelur.evaluate(Ypast[-numtestrecords:], Ycurr[-numtestrecords:], batch_size=128, verbose=1)
 
@@ -290,6 +317,18 @@ print('msemodelrmse', msemodelrmse)
 print('msemodelurmae', msemodelurmae)
 print('msemodelurmse', msemodelurmse)
 
+
+with open(MsemodelrmaePath, 'wb') as handle:
+    pickle.dump(msemodelrmae, handle, protocol=pickle.HIGHEST_PROTOCOL)
+
+with open(MsemodelrmsePath, 'wb') as handle:
+    pickle.dump(msemodelrmse, handle, protocol=pickle.HIGHEST_PROTOCOL)
+
+with open(MsemodelurmaePath, 'wb') as handle:
+    pickle.dump(msemodelurmae, handle, protocol=pickle.HIGHEST_PROTOCOL)
+
+with open(MsemodelurmsePath, 'wb') as handle:
+    pickle.dump(msemodelurmse, handle, protocol=pickle.HIGHEST_PROTOCOL)
 
 
 
